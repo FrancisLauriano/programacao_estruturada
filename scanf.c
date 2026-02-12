567559fe0d4d10e62bcbf42677bfedc5a17fa16d
FrancisLauriano <francislauriano@gmail.com>
2026-02-11 21:05:54 -0300

    uso printf e scanf

---
+++ scanf.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main(){
+	int idade = 0;
+	float peso = 0.0;
+	
+	printf("Digite uma idade:\n");
+	scanf("%d", &idade);
+	
+	printf("Digite um peso:\n");
+	scanf("%f", &peso);
+	
+	printf("\nIdade informada: %d anos.\n", idade);
+	printf("Peso informado: %.2f kg.\n", peso);
+	
+	return 0;
+}
+
+//int main(){
+//	
+//	int idade = 0;
+//	
+//	printf("Idade inicial: %d.\n", idade);
+//	
+//	printf("Digite sua idade:\n");
+//	scanf("%d", &idade);
+//	
+//	printf("\nIdade informada: %d.\n", idade);
+//	
+//	return 0;
+//}
+
+//int main(){
+//	
+//	int idade = 0;
+//	int ano = 1950;
+//	
+//	printf("Idade inicial: %d anos.\n", idade);
+//	printf("Ano inicial: %d.\n", ano);
+//	
+//	printf("Informe uma idade e o ano:\n");
+//	scanf("%d %d", &idade, &ano);
+//	
+//	printf("\nIdade informada: %d | Ano informado: %d.\n", idade, ano);
+//	
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
