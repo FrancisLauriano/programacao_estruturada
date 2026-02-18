04129cf3f6038365afa921fb3ee4b33cc9e8d51b
Francis Lauriano <130801505+FrancisLauriano@users.noreply.github.com>
2026-02-18 18:22:10 +0000

    ok

---
+++ operadores_logicos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float nota;
+
+    printf("Insira uma nota:\n");
+    scanf("%f", &nota);
+
+    if (nota >= 4.0 && nota < 7.0){
+        printf("Tem direito a exame!\n");
+    }
+
+    return 0;
+}
