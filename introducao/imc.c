1c7c33328231a60b7d85577d7d3e521026ea5650
Francis Lauriano <130801505+FrancisLauriano@users.noreply.github.com>
2026-02-23 01:33:29 +0000

    update

--- introducao/imc.c
+++ introducao/imc.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
 
 float calcularImc(float p, float a);
-char clasificacaoImc(float i);
+
+char* clasificacaoImc(float i);
 
 int main(){
     float peso, altura, imc;
+    char* classificacao;
 
     printf("Digite o peso:\n");
     scanf("%f", &peso);
 
+    if (peso <= 0.0){
+        printf("Peso Inválido!\n");
+        return 1;
+    }
+
     printf("Digite altura:\n");
     scanf("%f", &altura);
 
+    if (altura <= 0.0){
+        printf("Altura Inválida!\n");
+        return 1;
+    }
+
     imc = calcularImc(peso, altura);
 
+    classificacao = clasificacaoImc(imc); 
     printf("Seu IMC: %.2f.\n", imc);
+    printf("Classificacao: %s.\n", classificacao);
 
     return 0;
 }
 
-char classificacaoImc(float i){
-    if (i >= 18.00 && i <= 24.99) {
+char* clasificacaoImc(float i){
+    if(i < 18.5) {
+        return "Baixo Peso";
+    }else if(i <= 24.9) {
         return "Eutrofico";
-    }else if(i >=) 25 e < 30)
+    }else if(i <= 29.9) {
+        return "Sobrepeso";
+    }else if(i <= 39.9) {
+        return "Obesidade";
+    }else{
+        return "Obesidade Grave";
+    }
 }
 
 float calcularImc(float p, float a) {
